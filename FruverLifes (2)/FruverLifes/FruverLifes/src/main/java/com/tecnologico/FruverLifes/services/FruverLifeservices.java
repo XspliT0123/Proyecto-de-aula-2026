ecda6d5adc57f982d34e6b796959f10fba0e6d7f
XspliT0123 <alcidesj2003mg@gmail.com>
2026-03-27 17:58:32 -0500

    Add files via upload
    FruverLife y sus respectivas carpetas de models, templates etc.

---
+++ FruverLifes (2)/FruverLifes/FruverLifes/src/main/java/com/tecnologico/FruverLifes/services/FruverLifeservices.java
@@ -0,0 +1,11 @@
+package com.tecnologico.FruverLifes.services;
+
+import com.tecnologico.FruverLifes.model.FruverLifemodel;
+
+import java. util.List;
+
+public interface FruverLifeservices {
+   List<FruverLifemodel> listar();
+
+
+}
