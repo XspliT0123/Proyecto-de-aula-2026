1b4768929bba04a19af7e91bb3c1da2505ab565f
XspliT0123 <alcidesj2003mg@gmail.com>
2026-04-17 19:09:51 -0500

    Delete Proyecto FruverLife/ProyectoModificable/FruverLifes/FruverLifes/FruverLifes directory

--- Proyecto FruverLife/ProyectoModificable/FruverLifes/FruverLifes/FruverLifes/src/main/java/com/tecnologico/FruverLifes/Service/RutaService.java
+++
@@ -1,8 +0,0 @@
-package com.tecnologico.FruverLifes.Service;
-
-public class RutaService {
-        public static final String RUTA_USUARIOS ="C:\\Users\\carod\\Downloads\\ProyectoModificable2\\programasemanasanta\\semanasantaprograma\\FruverLifes\\FruverLifes\\FruverLifes\\src\\main\\resources\\templates\\usuarios.txt";
-
-        public static final String RUTA_CLIENTES ="C:\\Users\\carod\\Downloads\\ProyectoModificable2\\programasemanasanta\\semanasantaprograma\\FruverLifes\\FruverLifes\\FruverLifes\\src\\main\\resources\\templates\\clientes.txt";
-
-}
